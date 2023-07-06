98fae4bef4d5a9027245baa2f6ba9f8c24d862e5
Kuper <killkih@mail.ru>
2023-07-07 01:02:35 +0300

    Spec for index question

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'User can view the list of questions', %q{
+  In order to select question
+  As an authenticated user
+  I'd like to view a list of questions
+} do
+  given(:user) { create(:user) }
+  given!(:questions) { create_list(:question, 5) }
+
+  scenario 'Authenticated user views the list of questions' do
+    sign_in(user)
+
+    visit questions_path
+
+    expect(page).to have_content questions[1].title
+  end
+
+  scenario 'Unauthenticated user views the list of questions' do
+    visit questions_path
+
+    expect(page).to have_content questions[1].title
+  end
+end
+
