57512630bbe4ad0088d42d1da534b315d1f52fa3
Kuper <killkih@mail.ru>
2023-07-10 18:23:06 +0300

    Fix link_to + bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require popper
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,34 +1,39 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
 
-  expose :question
-  expose :answer
-
   def create
-    @exposed_answer = question.answers.new(answer_params)
-    @exposed_answer.user = current_user
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    @answer.user = current_user
 
-    if @exposed_answer.save
-      redirect_to question_path(question), notice: 'Answer added successfully!'
+    if @answer.save
+      redirect_to question_path(@question), notice: 'Answer added successfully!'
     else
-      redirect_to question_path(question), notice: 'Error adding answer'
+      redirect_to question_path(@question), notice: 'Error adding answer'
     end
   end
 
   def destroy
-      question = answer.question
+    @question = answer.question
 
     if current_user.id == answer.user_id
       answer.destroy
-      redirect_to question_path(question), notice: 'Answer successfully deleted!'
+      redirect_to question_path(@question), notice: 'Answer successfully deleted!'
     else
-      redirect_to question_path(question), notice: 'Only the author can delete a answer!'
+      redirect_to question_path(@question), notice: 'Only the author can delete a answer!'
     end
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body, :question_id)
   end
+
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  helper_method :answer
+
 end
