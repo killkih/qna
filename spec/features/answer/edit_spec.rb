4c0be06fdfe1e0152371f68a89cda0545cb83701
Kuper <killkih@mail.ru>
2023-07-18 02:12:17 +0300

    Only the author can edit their answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -7,6 +7,7 @@ feature 'User can edit his answer', %q{
 } do
 
   given!(:user) { create(:user) }
+  given(:other_user) { create(:user) }
   given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question, user: user) }
 
@@ -27,14 +28,32 @@ feature 'User can edit his answer', %q{
         fill_in 'Your answer', with: 'edited answer'
         click_on 'Save'
 
-        save_and_open_page
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
         expect(page).to_not have_selector 'textarea'
+        expect(page).to have_content 'edited answer'
+        expect(page).to_not have_content answer.body
       end
 
     end
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's question "
+
+    scenario 'edits his answer with errors' do
+      sign_in user
+      visit question_path(question)
+
+      click_on 'Edit'
+
+      within '.answers' do
+        fill_in 'Your answer', with: ''
+        click_on 'Save'
+      end
+
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario "tries to edit other user's question" do
+      sign_in other_user
+      visit question_path(question)
+
+      expect(page).to_not have_link 'Edit'
+    end
   end
 end
