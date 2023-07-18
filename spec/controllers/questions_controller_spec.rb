2b166f8fd1ce34835fe107c90f5bc4b495f62705
Kuper <killkih@mail.ru>
2023-07-18 03:27:58 +0300

    Only the author can edit their question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -77,35 +77,31 @@ RSpec.describe QuestionsController, type: :controller do
 
     context 'with valid attributes' do
       it 'assigns the requested question to @question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
+        patch :update, params: { id: question, question: attributes_for(:question), format: :js }
         expect(assigns(:question)).to eq question
       end
       it 'changes question attributes' do
-        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' }, format: :js }
         question.reload
 
         expect(question.title).to eq 'new title'
         expect(question.body).to eq 'new body'
       end
 
-      it 'redirects to updated question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
-        expect(response).to redirect_to question
+      it 'renders to updated question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }, format: :js
+        expect(response).to render_template :update
       end
     end
 
     context 'with invalid attributes' do
-      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) }, format: :js }
 
       it 'does not change question' do
         question.reload
 
         expect(question.title).to eq 'MyString'
         expect(question.body).to eq 'MyText'
-      end
-
-      it 're-renders edit view' do
-        expect(response).to render_template :edit
       end
     end
 
---
+++ spec/features/question/edit_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+feature 'User can edit his question', %q{
+  In order to correct mistakes
+  As an author of question
+  I'd like to be able to edit my question
+} do
+
+  given!(:user) { create(:user) }
+  given(:other_user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+
+  scenario 'Unauthenticated user can not edit question' do
+    visit question_path(question)
+
+    expect(page).to_not have_link 'Edit question'
+  end
+
+  describe 'Authenticated user', js: true do
+    scenario 'edits his question' do
+      sign_in user
+      visit question_path(question)
+
+      click_on 'Edit'
+
+      within '.question' do
+        fill_in 'Your question', with: 'edited question'
+        fill_in 'Body', with: 'test test test test'
+        click_on 'Save'
+
+
+        expect(page).to_not have_selector 'textarea'
+        expect(page).to have_content 'edited question'
+        expect(page).to_not have_content question.body
+        expect(page).to_not have_selector 'input'
+      end
+
+    end
+
+    scenario 'edits his question with errors' do
+      sign_in user
+      visit question_path(question)
+
+      click_on 'Edit'
+
+      within '.question' do
+        fill_in 'Your question', with: ''
+        fill_in 'Body', with: ''
+        click_on 'Save'
+      end
+
+      expect(page).to have_content "Title can't be blank"
+    end
+
+    scenario "tries to edit other user's question" do
+      sign_in other_user
+      visit question_path(question)
+
+      expect(page).to_not have_link 'Edit'
+    end
+  end
+end
