1fb9655991a55800f4cfe004d242d7116821f32f
Kuper <killkih@mail.ru>
2023-08-23 20:18:52 +0300

    Flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
   def hide_buttons(resource)
     'hidden' unless current_user.voted?(resource)
   end
+
+  def flash_message(argument)
+    return unless flash[argument].present?
+
+    content_tag :div, flash[argument], class: "flash #{argument}"
+  end
 end
