c90f6854f141538dbc3033395236311f4d6abba2
Kuper <killkih@mail.ru>
2023-09-05 20:52:51 +0300

    Add profiles index

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -4,7 +4,7 @@ describe 'Profiles API', type: :request do
   let(:headers) { { "CONTENT_TYPE" => "application/json",
                     "ACCEPT" => "application/json" } }
 
-  describe 'GET /api/v1/profile/me' do
+  describe 'GET /api/v1/profiles/me' do
     let(:api_path) { '/api/v1/profiles/me' }
 
     it_behaves_like 'API Authorizable' do
@@ -23,7 +23,47 @@ describe 'Profiles API', type: :request do
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+    let!(:profile) { create(:user) }
+    let!(:other_profile) { create(:user) }
+    let(:access_token) { create(:access_token, resource_owner_id: profile.id) }
+    let(:profile_response) { json['users'].first }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of profiles' do
+        expect(json['users'].size).to eq 1
+      end
+
+      it 'does not return requesting profile' do
+        expect(profile_response['id']).to_not eq profile.id.as_json
+      end
+
+      it 'returns all public fields' do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(profile_response[attr]).to eq other_profile.send(attr).as_json
         end
       end
 
