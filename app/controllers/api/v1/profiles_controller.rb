380f839b66d87cf8d307df634354b18e738a73d9
Kuper <killkih@mail.ru>
2023-09-05 20:52:51 +0300

    Add profiles index

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -6,6 +6,11 @@ class Api::V1::ProfilesController < Api::V1::BaseController
     render json: user
   end
 
+  def index
+    @users = User.where.not(id: user.id)
+    render json: @users
+  end
+
   private
 
   def user
