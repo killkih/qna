0b1050d53f61e454abfbc6aca8025ac35fdc3b2c
Kuper <killkih@mail.ru>
2023-09-10 16:24:37 +0300

    Question #create #update #destroy

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -12,9 +12,37 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     render json: question
   end
 
+  def create
+    @question = current_resource_owner.questions.new(question_params)
+
+    if @question.save
+      render json: @question
+    else
+      render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if question.update(question_params)
+      render json: question
+    else
+      render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    question.destroy
+  end
+
   private
 
   def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+    @question ||= Question.find(params[:id]) if params[:id]
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body,
+                                     links_attributes: %i[id name url _destroy],
+                                     reward_attributes: %i[title image])
   end
 end
