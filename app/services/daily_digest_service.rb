c4515e87086057a836c87095fb37977ead81d382
Kuper <killkih@mail.ru>
2023-09-18 18:49:36 +0300

    Screencast

---
+++ app/services/daily_digest_service.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class DailyDigestService
+
+  def send_digest
+    User.find_each(batch_size: 500) do |user|
+      DailyDigestMailer.digest(user).deliver_later
+    end
+  end
+end
