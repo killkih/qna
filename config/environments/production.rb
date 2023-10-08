3dd638a1d354dd63d0b991db16b111c93117d60c
Kuper <killkih@mail.ru>
2023-10-08 17:40:10 +0300

    add Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: '89.108.71.139' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp-relay.brevo.com',
+    port: 587,
+    user_name: Rails.application.credentials[Rails.env.to_sym][:mailer][:smtp_username],
+    password: Rails.application.credentials[Rails.env.to_sym][:mailer][:smtp_password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
