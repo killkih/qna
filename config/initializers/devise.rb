1d3eeb87b622df07f070c5a99971c1cee624a17b
Kuper <killkih@mail.ru>
2023-10-08 19:24:20 +0300

    fix mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '89.108.71.139'
+  config.mailer_sender = 'Q&A <admin@qna.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
